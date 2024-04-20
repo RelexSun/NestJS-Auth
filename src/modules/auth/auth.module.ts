62abe32ad76c5b390ffa8765838af69b29a290a3
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-04-20 21:24:23 +0700

    jwt secret

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,16 +4,20 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-    JwtModule.register({
-      secret: 'secret',
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
     }),
   ],
-
   controllers: [AuthController],
   providers: [AuthService],
 })
